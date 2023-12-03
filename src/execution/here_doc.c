e9b072c1a2b47ce051e54855ea99aa1d75cfbf9e
Maria Monpeat Lopez <mmonpeat@car9s6.42barcelona.com>
2023-12-03 15:53:24 +0100

    la llista de redis no es crea be, crec

--- src/execution/here_doc.c
+++ src/execution/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: mmonpeat <mmonpeat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 16:46:40 by mmonpeat          #+#    #+#             */
-/*   Updated: 2023/12/03 13:26:04 by mmonpeat         ###   ########.fr       */
+/*   Updated: 2023/12/03 15:49:29 by mmonpeat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	save_hd_fd(t_process *prcs)
 {
 	int		fd[2];
 
-    printf("save write pipe\n");
-	printf("1. fd_read_hd %i\n", prcs->fd_read_hd);
 	if (pipe(fd) == -1)
 		exit(1);
 	while (1)
@@ -26,10 +24,8 @@ void	save_hd_fd(t_process *prcs)
 		if (ft_strcmp(prcs->hd_line, prcs->rd->wrd) == 0)
 			break ;
 		write(fd[1], prcs->hd_line, ft_strlen(prcs->hd_line));
+		write(fd[1], "\n", 1);
 	}
-	dup2(prcs->fd_read_hd, STDOUT_FILENO);
-	close(fd[1]);
 	prcs->fd_read_hd = fd[0];
-	printf("2. fd_read_hd %i\n", prcs->fd_read_hd);
-	// close(prcs->fd_read_hd);
+	close(fd[1]);
 }
--- src/execution/redi.c
+++ src/execution/redi.c
@@ -16,21 +16,7 @@ void	redi_type(t_all *all, t_process *prcs, int fd_pipe[2])
 
 void	open_infile(t_process *prcs, int fd_pipe[2])
 {
-    // printf("i'm in open infile\n");
-    // if (access(prcs->rd->wrd, F_OK) == -1)
-	// {
-	// 	close_pipes(fd_pipe);
-	// 	printf("ERROR, redi infile\n");
-	// 	exit(1);//exit(ft_error(1, ERR_NFD, prcs->rd->wrd));
-	// }
 	prcs->rd->open = open(prcs->rd->wrd, O_RDONLY);
-	// if (access(prcs->rd->wrd, R_OK) == -1)
-	// {
-	// 	close_pipes(fd_pipe);
-	// 	printf("ERROR, redi infile\n");
-	// 	exit(1);
-	// 	//exit(ft_error(1, ERR_PERM, prcs->rd->wrd));
-	// }
 	dup2(prcs->rd->open, STDIN_FILENO);
 	close_pipes(fd_pipe);
 	close(prcs->rd->open);
@@ -38,22 +24,7 @@ void	open_infile(t_process *prcs, int fd_pipe[2])
 
 void	open_outfile(t_process *prcs, int fd_pipe[2])
 {
-	printf("i'm in open outfile\n");
 	prcs->rd->open = open(prcs->rd->wrd, O_WRONLY | O_TRUNC | O_CREAT, 0666);
-	// if (prcs->rd->open == -1)
-	// {
-	// 	close_pipes(fd_pipe);
-	// 	printf("ERROR, redi outfile\n");
-	// 	exit(1);
-	// 	//exit(ft_error(1, ERR_NFD, outfile));
-	// }
-	// if (access(prcs->rd->wrd, W_OK) == -1)
-	// {
-	// 	close_pipes(fd_pipe);
-	// 	printf("ERROR, redi outfile\n");
-	// 	exit(1);
-	// 	//exit(ft_error(1, ERR_PERM, outfile));
-	// }
 	dup2(prcs->rd->open, STDOUT_FILENO);
 	close_pipes(fd_pipe);
 	close(prcs->rd->open);
@@ -61,22 +32,7 @@ void	open_outfile(t_process *prcs, int fd_pipe[2])
 
 void apendd(t_process *prcs, int fd_pipe[2])
 {
-	printf("redi de apendd\n");
 	prcs->rd->open = open(prcs->rd->wrd, O_WRONLY | O_APPEND | O_CREAT, 0666);
-	// if (prcs->rd->open == -1)
-	// {
-	// 	close_pipes(fd_pipe);
-	// 	printf("ERROR, redi outfile\n");
-	// 	exit(1);
-	// 	//exit(ft_error(1, ERR_NFD, outfile));
-	// }
-	// if (access(prcs->rd->wrd, W_OK) == -1)
-	// {
-	// 	close_pipes(fd_pipe);
-	// 	printf("ERROR, redi outfile\n");
-	// 	exit(1);
-	// 	//exit(ft_error(1, ERR_PERM, outfile));
-	// }
 	dup2(prcs->rd->open, STDOUT_FILENO);
 	close_pipes(fd_pipe);
 	close(prcs->rd->open);
@@ -85,13 +41,11 @@ void apendd(t_process *prcs, int fd_pipe[2])
 void	here_doc(t_process *prcs, int fd_pipe[2])
 {
 	printf("llegir el fd_read_hd");
-	// (void)fd_pipe[2];
-	read(prcs->fd_read_hd, prcs->hd_line, ft_strlen(prcs->hd_line));
 	dup2(prcs->fd_read_hd, STDIN_FILENO);
 	close(prcs->fd_read_hd);
-
-	dup2(prcs->rd->open, STDIN_FILENO);
-	close_pipes(fd_pipe);
-	close(prcs->rd->open);
+	
+	// close(fd_pipe[1]);
+	dup2(fd_pipe[0], STDIN_FILENO);
+	close(fd_pipe[0]);
 }
 

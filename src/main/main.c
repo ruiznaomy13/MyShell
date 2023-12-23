7076f8efc9be2cbc18d9b0e7fe7418671ad24415
ruiznaomy13 <ruiznaomy13@gmail.com>
2023-12-23 12:21:08 +0100

    se hizo lo que se pudo

--- src/main/main.c
+++ src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: ncastell <ncastell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 12:06:27 by mmonpeat          #+#    #+#             */
-/*   Updated: 2023/12/22 17:03:44 by ncastell         ###   ########.fr       */
+/*   Updated: 2023/12/23 12:11:06 by ncastell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,61 +47,3 @@ int	main(int ac, char **av, char **env)
 	loop(&all);
 	return (0);
 }
-
-// void	free_env_list(t_env *env)
-// {
-// 	t_env	*temp;
-
-// 	while (env != NULL)
-// 	{
-// 		temp = env;
-// 		env = env->next;
-// 		free(temp->key);
-// 		free(temp->value);
-// 		free(temp);
-// 	}
-// }
-
-// void	free_token_list(t_token *token)
-// {
-// 	t_token	*temp;
-
-// 	while (token != NULL)
-// 	{
-// 		temp = token;
-// 		token = token->next;
-// 		free(temp->wrd);
-// 		free(temp);
-// 	}
-// }
-
-// void	free_rd(t_process *prc)
-// {
-// 	t_token	*aux_rd;
-
-// 	while (prc != NULL && prc->rd != NULL)
-// 	{
-// 		aux_rd = prc->rd;
-// 		prc->rd = prc->rd->next;
-// 		free(aux_rd);
-// 	}
-// 	prc->rd = NULL;
-// }
-
-// void	ft_free_all(t_all *all)
-// {
-// 	t_token		*tkn;
-// 	t_process	*prc;
-
-// 	while (all->prcs != NULL)
-// 	{
-// 		prc = all->prcs;
-// 		all->prcs = all->prcs->next;
-// 		free_rd(prc);
-// 		free(prc);
-// 		prc = NULL;
-// 	}
-// 	all->prcs = NULL;
-// 	all->token = NULL;
-// }
-

9dff25dbaeae6040e77cb79f76736b9e8531f2d3
Maria Monpeat Lopez <mmonpeat@car10s2.42barcelona.com>
2023-12-24 13:12:21 +0100

    sense comandes de leaks

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: mmonpeat <mmonpeat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 22:28:07 by ncastell          #+#    #+#             */
-/*   Updated: 2023/12/24 10:56:30 by mmonpeat         ###   ########.fr       */
+/*   Updated: 2023/12/24 13:12:02 by mmonpeat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ int	exit_prog(t_all *all, int output)
 {
 	clear_history();
 	ft_free_all(all, E_EXIT);
-	system("leaks minishell");
 	exit(all->error);
 	return (output);
 }
